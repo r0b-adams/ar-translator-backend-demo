45716787f61f05e5f2f4bb7d5e01997ec083569b
Robert Adams <adamsiii.robert@gmail.com>
2022-07-21 18:09:46 -0700

    deleted duplicate files

--- public/script.js
+++
@@ -1,9 +0,0 @@
-let languages;
-
-const getLanguages = async () => {
-  const res = await fetch('/languages');
-  const data = await res.json();
-  languages = data;
-};
-
-window.onload = getLanguages;
