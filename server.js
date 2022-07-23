af6d7fe399f2d53e025442f5148faaf4a3eb6016
Robert Adams <adamsiii.robert@gmail.com>
2022-07-23 10:47:16 -0700

    adjusted max allowed file upload size

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
-require('dotenv').config();
-
 const express = require('express');
 const routes = require('./routes');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static('client')); // UI files
+const FILE_UPLOAD_LIMIT = '100mb';
+
+app.use(
+  express.urlencoded({
+    limit: FILE_UPLOAD_LIMIT,
+    extended: false,
+  })
+);
+app.use(express.json({ limit: FILE_UPLOAD_LIMIT }));
+
+// UI files (by default, looks for 'index.html')
+// https://expressjs.com/en/4x/api.html#express.static
+// handles the '/' root lvl endpoint
+app.use(express.static('client'));
+
 app.use(routes);
 
-// catch all error
-app.get('*', (_req, res) => res.status(404).json({ error: 'not found' }));
+// catch all for unrecognized endpoints
+app.get('*', (_req, res) => {
+  res.status(404).json({ error: 'not found' });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
