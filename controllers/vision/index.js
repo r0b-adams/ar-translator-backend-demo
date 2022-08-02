2538fa1899cb3830ab653f064ea1eccad5435e03
Robert Adams <adamsiii.robert@gmail.com>
2022-08-01 17:30:32 -0700

    add controller to rtn objs with translation

--- controllers/vision/index.js
+++ controllers/vision/index.js
@@ -1,4 +1,4 @@
-const { viewer } = require('../../google_apis/clients');
+const { viewer, translator } = require('../../google_apis/clients');
 
 const analyzeObjects = async (req, res) => {
   try {
@@ -19,4 +19,43 @@ const analyzeObjects = async (req, res) => {
   }
 };
 
-module.exports = { analyzeObjects };
+const localizeObjectsAndTranslate = async (req, res) => {
+  try {
+    const { img, to } = req.body;
+    const [, b64encodedImage] = img.split(',');
+
+    const request = {
+      image: { content: b64encodedImage },
+    };
+
+    const [result] = await viewer.objectLocalization(request);
+    const objects = result.localizedObjectAnnotations;
+
+    if (!objects.length) res.status(200).json({ message: 'no objects found' });
+    const objNames = objects.map((obj) => obj.name);
+
+    // call Google Translate *once* with an array of object names
+    const [translations] = await translator.translate(objNames, {
+      from: 'en',
+      to,
+    });
+
+    // return objects array with source, target, and translation included
+    const objsWithTranslations = objects.map((obj, idx) => {
+      delete obj.languageCode; // this field is always empty
+      return {
+        ...obj,
+        srcLangCode: from,
+        trgLangCode: to,
+        translatedName: translations[idx],
+      };
+    });
+
+    res.status(200).json(objsWithTranslations);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
+  }
+};
+
+module.exports = { analyzeObjects, localizeObjectsAndTranslate };
