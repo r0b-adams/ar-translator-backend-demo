196a61f1ea809f2c6ae76a1b8fdc77a8f2e3ba90
Robert Adams <adamsiii.robert@gmail.com>
2022-08-01 17:18:18 -0700

    added logic to pop sel opts

--- client/script.js
+++ client/script.js
@@ -1,13 +1,14 @@
-let languages;
 let blobURL;
+let defaultLangCode;
 
 const input = document.getElementById('input');
 const preview = document.getElementById('preview');
 const output = document.getElementById('output');
+const trgLangSelect = document.getElementById('trg');
 
 input.onchange = (event) => {
   try {
-    if (blobURL) URL.revokeObjectURL(blobURL);
+    if (blobURL) URL.revokeObjectURL(blobURL); // remove ref to any previously uploaded file
     if (event.target.files.length) {
       const files = event.target.files;
       blobURL = URL.createObjectURL(files[0]);
@@ -37,7 +38,7 @@ const fetchImageObjects = async (e) => {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ img: b64img }),
+      body: JSON.stringify({ img: b64img, to: trgLangSelect.value }),
     });
     const data = await response.json();
     output.textContent = JSON.stringify(data, undefined, 2);
@@ -49,7 +50,24 @@ const fetchImageObjects = async (e) => {
 const getLanguages = async () => {
   const response = await fetch('/translateAPI/languages', { method: 'GET' });
   const data = await response.json();
-  languages = data;
+  return data;
 };
 
-window.onload = getLanguages;
+const populateOptions = async () => {
+  const { languages } = await getLanguages();
+
+  if (languages.length) {
+    for (let i = 0; i < languages.length; i++) {
+      const { code, name } = languages[i];
+
+      const trgOpt = document.createElement('option');
+      trgOpt.textContent = `${code} - ${name}`;
+      trgOpt.value = code;
+      trgOpt.setAttribute('data-code', code);
+      trgOpt.setAttribute('data-name', name);
+      trgLangSelect.append(trgOpt);
+    }
+  }
+};
+
+window.onload = async () => await populateOptions();
