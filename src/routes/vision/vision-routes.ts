c67f2d461c563a90c481e3a4af4f2806566c4d1e
Robert Adams <adamsiii.robert@gmail.com>
2022-08-03 16:47:47 -0700

    removed client files

--- src/routes/vision/vision-routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { localizeAndTranslate } from '../../controllers/vision';
-
-const router = Router();
-router.post('/objects', localizeAndTranslate);
-
-export default router;
