c7110bf08a46e232b6ea9a7dc253dfe102b712b0
Robert Adams <adamsiii.robert@gmail.com>
2022-08-03 16:55:35 -0700

    name update

--- src/routes/translate/translate-routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-const { postTranslate, getLanguages } = require('../../controllers/translate');
-
-const router = Router();
-router.get('/languages', getLanguages);
-router.post('/translate', postTranslate);
-
-export default router;
