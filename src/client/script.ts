27f1ccd7b8ed6a58dc93de6220856c83f0e99f5e
Robert Adams <adamsiii.robert@gmail.com>
2022-08-03 00:27:00 -0700

    moved from root

---
+++ src/client/script.ts
@@ -0,0 +1,81 @@
+import { LanguageResult } from '@google-cloud/translate/build/src/v2';
+
+let blobURL: string;
+const defaultLangCode = navigator.language;
+
+const trgLangSelect = document.getElementById('trg') as HTMLSelectElement;
+const input = document.getElementById('input')! as HTMLInputElement;
+const preview = document.getElementById('preview') as HTMLImageElement;
+const output = document.getElementById('output') as HTMLTextAreaElement;
+
+input.onchange = (_e): void => {
+  try {
+    // https://developer.mozilla.org/en-US/docs/Web/API/URL#static_methods
+    if (blobURL) URL.revokeObjectURL(blobURL); // remove ref to previously uploaded file
+    if (input.files) {
+      const files = input.files;
+      blobURL = URL.createObjectURL(files[0]);
+      preview.src = blobURL;
+      readImage(files[0]);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// https://developer.mozilla.org/en-US/docs/Web/API/FileReader
+const reader = new FileReader();
+const readImage = (file: File): void => {
+  try {
+    reader.onload = fetchImageObjects;
+    reader.readAsDataURL(file);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const fetchImageObjects = async (
+  e: ProgressEvent<FileReader>
+): Promise<void> => {
+  try {
+    if (!e.target) return;
+    const b64img = e.target.result;
+    const response = await fetch('/visionAPI/objects', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ img: b64img, to: trgLangSelect.value }),
+    });
+    const data = await response.json();
+    output.textContent = JSON.stringify(data, undefined, 2);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+type Languages = { languages: LanguageResult[] };
+const loadLanguages = async () => {
+  const response = await fetch('/translateAPI/languages', { method: 'GET' });
+  const data: Promise<Languages> = await response.json();
+  return data;
+};
+
+const populateOptions = async (): Promise<void> => {
+  const { languages } = await loadLanguages();
+
+  if (languages.length) {
+    for (let i = 0; i < languages.length; i++) {
+      const { code, name } = languages[i];
+
+      const trgOpt = document.createElement('option');
+      trgOpt.textContent = `${code} - ${name}`;
+      trgOpt.value = code;
+      trgOpt.setAttribute('data-code', code);
+      trgOpt.setAttribute('data-name', name);
+      trgLangSelect.append(trgOpt);
+    }
+  }
+};
+
+window.onload = async () => await populateOptions();
