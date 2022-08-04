d2b5851fb04414edd9020e420fbad0e777a045a0
Robert Adams <adamsiii.robert@gmail.com>
2022-08-03 19:30:14 -0700

    added auth endpoints

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import authEndpoints from './auth';
 import translateAPI from './translate';
 import visionAPI from './vision';
 
 const router = Router();
 
+router.use(authEndpoints);
 router.use(translateAPI);
 router.use(visionAPI);
 
