971a0e9c4efb81dfa4676d297b578e741e7a4491
Robert Adams <adamsiii.robert@gmail.com>
2022-08-08 22:08:24 -0700

    renamed files

--- src/google_apis/clients/_annotator.ts
+++
@@ -1,6 +0,0 @@
-import { ImageAnnotatorClient } from '@google-cloud/vision';
-import keyFilename from '../credentials';
-
-const annotator = new ImageAnnotatorClient({ keyFilename });
-
-export default annotator;
--- src/google_apis/clients/_translator.ts
+++
@@ -1,7 +0,0 @@
-import { v2 } from '@google-cloud/translate';
-import keyFilename from '../credentials';
-
-const { Translate } = v2;
-const translator = new Translate({ keyFilename });
-
-export default translator;
