4d2bbfb7b68815ad034f65fdee597b026b910358
Robert Adams <adamsiii.robert@gmail.com>
2022-08-08 22:24:21 -0700

    modified comment

--- src/middleware/authenticate.ts
+++ src/middleware/authenticate.ts
@@ -4,7 +4,7 @@ import { RequestHandler } from 'express';
 const { SECRET_KEY } = process.env;
 
 // intercept the req object before reaching any endpoints
-// if there is a valid token, assign it to a req object property
+// if there is a valid token, assign it to req object property
 const authenticate: RequestHandler = (req, _res, next) => {
   const bearer = req.headers.authorization;
   const token = bearer?.split(' ')[1];
