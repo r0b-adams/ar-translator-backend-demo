8ba2e230a5b631c55bf786fde23136cb1d819b3f
Robert Adams <adamsiii.robert@gmail.com>
2022-08-08 23:45:22 -0700

    tweaked exports and casing

--- src/db/models/User/validation.ts
+++ src/db/models/User/validation.ts
@@ -1,20 +1,21 @@
-import Joi from 'Joi';
+import joi from 'joi';
 import { Constants } from './constants';
 
 const { MAX_LENGTH, MIN_LENGTH, PASSWORD_REGEX } = Constants;
 
-const Joi_userSchema = Joi.object({
-  username: Joi.string().token().required(),
-  password: Joi.string()
+export const joi_userSchema = joi.object({
+  username: joi.string().token().required(),
+  password: joi
+    .string()
     .min(MIN_LENGTH)
     .max(MAX_LENGTH)
     .pattern(PASSWORD_REGEX)
     .required(),
-  email: Joi.string().email().required(),
+  email: joi.string().email().required(),
 });
 
-const handleValidate = (type: string) => (payload: string) => {
-  const schema = Joi_userSchema.extract(type);
+export const handleValidate = (type: string) => (payload: string) => {
+  const schema = joi_userSchema.extract(type);
   const { error } = schema.validate(payload);
   return error ? false : true; // error undefined if payload is valid
 };
