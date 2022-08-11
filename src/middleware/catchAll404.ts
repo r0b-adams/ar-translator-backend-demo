16561f6054e4bb505a4262a478c9da59ee760f1d
Robert Adams <adamsiii.robert@gmail.com>
2022-08-11 00:43:36 -0700

    updated response body

--- src/middleware/catchAll404.ts
+++ src/middleware/catchAll404.ts
@@ -1,7 +1,7 @@
 import { RequestHandler } from 'express';
 
 const catchAll404: RequestHandler = (_, res) => {
-  res.status(404).send("Sorry can't find that!");
+  res.status(404).send({ message: "Sorry can't find that!" });
 };
 
 export default catchAll404;
