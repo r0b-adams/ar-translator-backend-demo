8c2087cf029eca63a53e1056b57045784fa9b63e
Robert Adams <adamsiii.robert@gmail.com>
2022-08-11 00:22:07 -0700

    added validation and err handling

--- src/controllers/translate/index.ts
+++ src/controllers/translate/index.ts
@@ -3,6 +3,7 @@ import { RequestHandler } from 'express';
 import ReqBody from '../../@types/requests';
 import ResBody from '../../@types/responses';
 import Joi from 'joi';
+import { AuthError } from '../../helpers/errors';
 
 /*
  * GET /translateAPI/languages
@@ -12,14 +13,21 @@ export const getLanguages: RequestHandler<{}, {}, ResBody.Languages> = async (
   res
 ) => {
   try {
-    if (!req.userID) {
-      res.status(401).json({ error: 'Please login' });
-      return;
-    }
+    // check authorization
+    if (!req.userID) throw new AuthError('Please login');
+
+    // query Google Translte API
     const [languages] = await translator.getLanguages();
     res.status(200).json(languages);
   } catch (error) {
-    res.status(500).json({ error });
+    // no credentials
+    if (error instanceof AuthError) {
+      res.status(401).json({ error: error.message });
+
+      // misc
+    } else if (error instanceof Error) {
+      res.status(500).json({ error: error.message });
+    }
   }
 };
 
@@ -32,22 +40,36 @@ export const postTranslate: RequestHandler<
   ReqBody.Translate
 > = async (req, res) => {
   try {
-    if (!req.userID) {
-      res.status(401).json({ error: 'Please login' });
-      return;
-    }
+    // check authorization
+    if (!req.userID) throw new AuthError('Please login');
+
+    // validate request body
+    const schema = Joi.object({
+      text: Joi.string().required(),
+      from: Joi.string().required().max(2),
+      to: Joi.string().required().max(2),
+    });
+    const { error } = schema.validate(req.body, {
+      abortEarly: false,
+    });
+    if (error) throw error;
+
+    // query Google Translate API
     const { text, from, to } = req.body;
-
-    const body = Joi.object({
-      text: Joi.string(),
-      from: Joi.string(),
-      to: Joi.string()
-    })
-
-
     const [result] = await translator.translate(text, { from, to });
     res.status(200).json({ result });
   } catch (error) {
-    res.status(500).json({ error });
+    //no credentials
+    if (error instanceof AuthError) {
+      res.status(401).json({ error: error.message });
+
+      // Joi errors
+    } else if (error instanceof Joi.ValidationError) {
+      res.status(400).json({ error: error.message });
+
+      // misc
+    } else if (error instanceof Error) {
+      res.status(500).json({ error: error.message });
+    }
   }
 };
