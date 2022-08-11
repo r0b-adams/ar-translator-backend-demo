35a7e9914b2ee6dc2e6baf929cc82f2cf0ffc285
Robert Adams <adamsiii.robert@gmail.com>
2022-08-11 16:51:50 -0700

    reshaped for exports

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,5 +1,6 @@
-export const Constants = {
-  MIN_LENGTH: 12,
-  SALT_ROUNDS: 10,
-  PASSWORD_REGEX: /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{12,64}$/,
-};
+const MIN_LENGTH = 12;
+const SALT_ROUNDS = 10;
+const PASSWORD_REGEX =
+  /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{12,64}$/;
+
+export { MIN_LENGTH, SALT_ROUNDS, PASSWORD_REGEX };
