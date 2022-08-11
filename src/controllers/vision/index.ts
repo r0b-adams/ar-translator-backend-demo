b17d663ba56cfa1cd8022e435ed27d65503d6134
Robert Adams <adamsiii.robert@gmail.com>
2022-08-11 16:50:48 -0700

    moved authorization and validation to middleware

--- src/controllers/vision/index.ts
+++ src/controllers/vision/index.ts
@@ -1,10 +1,9 @@
-import { annotator, translator } from '../../google_apis/clients';
-
 import { RequestHandler } from 'express';
+import Joi from 'joi';
+
+import { annotator, translator } from '../../google_APIs/clients';
 import ReqBody from '../../@types/requests';
 import ResBody from '../../@types/responses';
-import Joi from 'joi';
-import { AuthError } from '../../helpers/errors';
 
 export const localizeAndTranslate: RequestHandler<
   {},
@@ -12,24 +11,13 @@ export const localizeAndTranslate: RequestHandler<
   ReqBody.Vision
 > = async (req, res) => {
   try {
-    // check authorization
-    if (!req.userID) throw new AuthError('Please login');
-
-    // validate request body
-    const schema = Joi.object({
-      img: Joi.string().dataUri(),
-      to: Joi.string().max(2),
-    });
-    const { error: reqErr } = schema.validate(req.body);
-    if (reqErr) throw reqErr;
-
     // grab target language and encoded data
     const { img, to } = req.body; // img arrives as a string of b64 encoded data
     const [, b64encodedImage] = img.split(','); // ignore the MIME type and grab the b64 substr
 
     // validate encoded img data
-    const { error: b64Err } = Joi.string().base64().validate(b64encodedImage);
-    if (b64Err) throw b64Err;
+    const { error } = Joi.string().base64().validate(b64encodedImage);
+    if (error) throw error;
 
     // query Google Vision API
     const [result] = await annotator.objectLocalization!({
@@ -37,13 +25,13 @@ export const localizeAndTranslate: RequestHandler<
     });
     const objects = result.localizedObjectAnnotations;
 
-    // no objects recognized :(
+    // no objects recognized
     if (!objects || !objects.length) {
-      res.status(200).json({ message: 'no objects found' });
+      res.status(204).json({ message: 'no objects found' });
       return;
     }
 
-    // Google Vision only returns results in English
+    // Google Vision only returns English results
     // if that is the target, just return data
     if (to === 'en') {
       res.status(200).json(objects);
@@ -69,16 +57,7 @@ export const localizeAndTranslate: RequestHandler<
 
     res.status(200).json(objsWithTranslations);
   } catch (error) {
-    //no credentials
-    if (error instanceof AuthError) {
-      res.status(401).json({ error: error.message });
-
-      // Joi errors
-    } else if (error instanceof Joi.ValidationError) {
-      res.status(400).json({ error: error.message });
-
-      // misc
-    } else if (error instanceof Error) {
+    if (error instanceof Error) {
       res.status(500).json({ error: error.message });
     }
   }
