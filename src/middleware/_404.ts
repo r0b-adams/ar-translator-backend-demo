52ca4bea531f8b5a0507a4aa566082c0d0d4a219
Robert Adams <adamsiii.robert@gmail.com>
2022-08-11 16:49:14 -0700

    renamed

--- src/middleware/_404.ts
+++ src/middleware/_404.ts
@@ -1,7 +1,7 @@
 import { RequestHandler } from 'express';
 
-const catchAll404: RequestHandler = (_, res) => {
+const _404: RequestHandler = (_, res) => {
   res.status(404).send({ message: "Sorry can't find that!" });
 };
 
-export default catchAll404;
+export default _404;
