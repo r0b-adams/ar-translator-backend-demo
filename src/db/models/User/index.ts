3b841d1061f350fed6efb381670b8b93c8908b33
Robert Adams <adamsiii.robert@gmail.com>
2022-08-11 00:23:42 -0700

    delete Mongoose schema lvl validation, updated pre

--- src/db/models/User/index.ts
+++ src/db/models/User/index.ts
@@ -1,12 +1,11 @@
 import bcrypt from 'bcrypt';
 import { model, Model, Schema, Document } from 'mongoose';
-import { handleValidate, joi_userSchema } from './validation';
-import { Constants, PayloadType } from './constants';
 
-const { MAX_LENGTH, MIN_LENGTH, SALT_ROUNDS } = Constants;
-const { USERNAME, PASSWORD, EMAIL } = PayloadType;
+import { Constants } from './constants';
 
-interface User extends Document {
+const { SALT_ROUNDS } = Constants;
+
+export interface UserDoc extends Document {
   username: string;
   password: string;
   email: string;
@@ -14,48 +13,29 @@ interface User extends Document {
   updatedAt: Date;
 }
 
-const userSchema = new Schema<User>(
+const userSchema = new Schema<UserDoc>(
   {
     username: {
       type: String,
-      required: [true, 'username is required'],
       trim: true,
-      maxlength: [MAX_LENGTH, `maximum username length is ${MAX_LENGTH}`],
-      validate: {
-        validator: handleValidate(USERNAME),
-        message: (_props) => 'invalid username',
-      },
     },
     email: {
       type: String,
-      required: [true, 'email is required'],
       trim: true,
-      validate: {
-        validator: handleValidate(EMAIL),
-        message: (_props) => `invalid email`,
-      },
     },
     password: {
       type: String,
-      required: [true, 'password is required'],
       trim: true,
-      minlength: [MIN_LENGTH, `password length must be at least ${MIN_LENGTH}`],
-      maxlength: [MAX_LENGTH, `maximum password length is ${MAX_LENGTH}`],
-      validate: {
-        validator: handleValidate(PASSWORD),
-        message: (_props) => 'invalid password',
-      },
     },
   },
   // options
   { timestamps: true }
 );
 
-userSchema.pre<User>('save', async function () {
-  this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
+userSchema.pre<UserDoc>('save', async function () {
+  if (this.isNew) this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
 });
 
-const User: Model<User> = model('User', userSchema);
+const User: Model<UserDoc> = model('User', userSchema);
 
-export { handleValidate, joi_userSchema };
 export default User;
