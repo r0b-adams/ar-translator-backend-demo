48d20c761f3f2d7466ee2fc8e5480839bd95de8c
Robert Adams <adamsiii.robert@gmail.com>
2022-08-11 22:14:02 -0700

    added dictionary to lookup schema

---
+++ src/validation/schema.ts
@@ -0,0 +1,35 @@
+import Joi from 'joi';
+
+import { MIN_LENGTH, PASSWORD_REGEX } from '../helpers/constants';
+
+enum ReqType {
+  POST_REGISTER = 'POST/auth/register',
+  POST_LOGIN = 'POST/auth/login',
+  POST_TRANSLATE = 'POST/translateAPI/translate',
+  POST_OBJECTS = 'POST/visionAPI/objects',
+}
+
+export const RequestBodySchema: { [key: string]: Joi.ObjectSchema<any> } = {};
+
+RequestBodySchema[ReqType.POST_REGISTER] = Joi.object({
+  email: Joi.string().email().required(),
+  username: Joi.string().required(),
+  password: Joi.string().min(MIN_LENGTH).pattern(PASSWORD_REGEX).required(),
+});
+
+RequestBodySchema[ReqType.POST_LOGIN] = Joi.object({
+  username: Joi.string().required(),
+  password: Joi.string().required(),
+});
+
+RequestBodySchema[ReqType.POST_TRANSLATE] = Joi.object({
+  email: Joi.string().email().required(),
+  username: Joi.string().required(),
+  password: Joi.string().min(MIN_LENGTH).pattern(PASSWORD_REGEX).required(),
+});
+
+RequestBodySchema[ReqType.POST_OBJECTS] = Joi.object({
+  email: Joi.string().email().required(),
+  username: Joi.string().required(),
+  password: Joi.string().min(MIN_LENGTH).pattern(PASSWORD_REGEX).required(),
+});
