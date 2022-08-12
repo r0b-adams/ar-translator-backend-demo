9a2d0148a95bd622c98150ad163d8f6104a7efcf
Robert Adams <adamsiii.robert@gmail.com>
2022-08-11 22:16:18 -0700

    moved err handling to own middleware

--- src/controllers/translate/index.ts
+++ src/controllers/translate/index.ts
@@ -1,42 +1,29 @@
-import Joi from 'joi';
 import { RequestHandler } from 'express';
 
 import { translator } from '../../google_APIs/clients';
-import ReqBody from '../../@types/requests';
-import ResBody from '../../@types/responses';
 
 /*
  * GET /translateAPI/languages
  */
-export const getLanguages: RequestHandler<{}, {}, ResBody.Languages> = async (
-  _req,
-  res
-) => {
+export const getLanguages: RequestHandler = async (_req, res, next) => {
   try {
     const [languages] = await translator.getLanguages();
     res.status(200).json(languages);
-  } catch (error) {
-    if (error instanceof Error) {
-      res.status(500).json({ error: error.message });
-    }
+  } catch (err) {
+    next(err);
   }
 };
 
 /*
  * POST /translateAPI/translate
  */
-export const postTranslate: RequestHandler<
-  {},
-  ResBody.Translation,
-  ReqBody.Translate
-> = async (req, res) => {
+export const postTranslate: RequestHandler = async (req, res, next) => {
   try {
     const { text, from, to } = req.body;
+
     const [result] = await translator.translate(text, { from, to });
     res.status(200).json({ result });
-  } catch (error) {
-    if (error instanceof Error) {
-      res.status(500).json({ error: error.message });
-    }
+  } catch (err) {
+    next(err);
   }
 };
