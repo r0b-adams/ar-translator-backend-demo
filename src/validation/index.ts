8be0e315154a2c73fe5d565a1e2be0f77c30f40a
Robert Adams <adamsiii.robert@gmail.com>
2022-08-12 01:28:03 -0700

    moved logic back to middleware

--- src/validation/index.ts
+++ src/validation/index.ts
@@ -1,19 +1,3 @@
-import { Request } from 'express';
-
 import { RequestBodySchema } from './schema';
 
-const validateRequestBody = async (req: Request): Promise<void> => {
-  const { method, originalUrl, body } = req;
-
-  // lookup schema by method and endpoint
-  // (e.g. POST/auth/register)
-  const reqTypeKey = method + originalUrl;
-  const schema = RequestBodySchema[reqTypeKey];
-
-  const { error } = await schema.validateAsync(body, {
-    abortEarly: false,
-  });
-  if (error) throw error;
-};
-
-export { validateRequestBody };
+export default RequestBodySchema;
