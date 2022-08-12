b3e54f7ac6bfe2cf3f58dde11ef2d6b5ae3ba405
Robert Adams <adamsiii.robert@gmail.com>
2022-08-11 22:13:27 -0700

    added middleware to register endpoint

--- src/routes/auth/routes.ts
+++ src/routes/auth/routes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 
 import { getUser, register, login, logout } from '../../controllers/auth';
-import { authorize, validate } from '../../middleware';
+import { authorize, validate, checkUniqueness } from '../../middleware';
 
 const router = Router();
 
 router.get('/users', authorize, getUser);
-router.post('/register', validate, register);
+router.post('/register', validate, checkUniqueness, register);
 router.post('/login', validate, login);
 router.delete('/logout', authorize, logout);
 
