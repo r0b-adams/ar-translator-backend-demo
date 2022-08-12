de0c823733ac80ed5e8cea2dd27932868aa78e17
Robert Adams <adamsiii.robert@gmail.com>
2022-08-12 01:25:07 -0700

    reverted

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -1,16 +1,32 @@
 import { RequestHandler } from 'express';
+import { ValidationError } from 'Joi';
 
-import { validateRequestBody } from '../validation';
+import RequestBodySchema from '../validation';
 
 /**
  * verify shape of request body
  */
-const validate: RequestHandler = async (req, _res, next) => {
+const validate: RequestHandler = (req, _res, next) => {
   try {
-    await validateRequestBody(req);
+    const { method, originalUrl, body } = req;
+
+    // lookup schema by method and endpoint
+    // (e.g. POST/auth/register)
+    const reqTypeKey = method + originalUrl;
+    const schema = RequestBodySchema[reqTypeKey];
+
+    const { error } = schema.validate(body, {
+      abortEarly: false, // collect all errors
+    });
+
+    // coerce error to a ValidationError instance
+    if (error) {
+      throw new ValidationError(error.message, error.details, error._original);
+    }
+
     next();
-  } catch (error) {
-    next(error);
+  } catch (err) {
+    next(err);
   }
 };
 
