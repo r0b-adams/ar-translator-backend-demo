55453a9acdd70b7b55f09d3712087ab612bc95e6
Robert Adams <adamsiii.robert@gmail.com>
2022-08-11 22:20:08 -0700

    added middleware to check if un or email exist

---
+++ src/middleware/checkUniqueness.ts
@@ -0,0 +1,25 @@
+import { RequestHandler } from 'express';
+import { Types } from 'mongoose';
+
+import { User, UserDoc } from '../db/models';
+import { UniquenessError } from '../helpers/errors';
+
+const checkUniqueness: RequestHandler = async (req, _res, next) => {
+  try {
+    let user:
+      | (UserDoc & {
+          _id: Types.ObjectId;
+        })
+      | null;
+
+    user = await User.findOne({ username: req.body.username });
+    if (user) throw new UniquenessError('that username already exists');
+
+    user = await User.findOne({ email: req.body.email });
+    if (user) throw new UniquenessError('user with that email already exists');
+  } catch (err) {
+    next(err);
+  }
+};
+
+export default checkUniqueness;
