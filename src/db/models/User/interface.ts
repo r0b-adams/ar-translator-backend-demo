8147ab5cba742fb8f71549b69c6b873e8854177b
Robert Adams <adamsiii.robert@gmail.com>
2022-08-14 18:41:54 -0700

    renamed file

--- src/db/models/User/interface.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from 'mongoose';
-
-export interface UserDoc extends Document {
-  username: string;
-  password: string;
-  email: string;
-  createdAt: Date;
-  updatedAt: Date;
-}
