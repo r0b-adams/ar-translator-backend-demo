daa796a7ee65fe4354cf63c7d14c20ecc86f898f
Robert Adams <adamsiii.robert@gmail.com>
2022-08-15 11:55:42 -0700

    renamed file

--- src/middleware/respond404.ts
+++ src/middleware/respond404.ts
@@ -1,7 +1,7 @@
 import { RequestHandler } from 'express';
 
-const _404: RequestHandler = (_, res) => {
+const respond404: RequestHandler = (_, res) => {
   res.status(404).send({ message: "Sorry can't find that!" });
 };
 
-export default _404;
+export default respond404;
