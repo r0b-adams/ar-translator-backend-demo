03fb9685cf1220c1431fcefe30e96e08f280f111
Robert Adams <adamsiii.robert@gmail.com>
2022-08-15 12:02:14 -0700

    added static methods for custom queries

--- src/db/models/User/index.ts
+++ src/db/models/User/index.ts
@@ -2,12 +2,33 @@ import { Schema, model } from 'mongoose';
 
 import profile from './_profile';
 import credentials from './_credentials';
-import { UserDoc, UserModel } from './interfaces';
+import { UserDoc, UserModel } from './types';
 
 const userSchema = new Schema<UserDoc, UserModel>({
   profile,
   credentials,
 });
+
+// Model lvl static methods
+
+userSchema.static('findByEmail', async function findByEmail(value: string) {
+  return this._byProfProp('email', value);
+});
+
+userSchema.static('findByUsername', async function findByUsername(value: string) {
+  return this._byProfProp('username', value);
+});
+
+userSchema.static(
+  '_byProfProp',
+
+  // helper to create queries by profile subdoc property names
+  async function _byProfProp(property: string, value: string) {
+    const conditions: { [key: string]: string } = {};
+    conditions[`profile.${property}`] = value;
+    return this.findOne(conditions);
+  }
+);
 
 const User = model<UserDoc, UserModel>('user', userSchema);
 
