bf0d37a8b8738d21e8fd46ca68eae896342f985d
Robert Adams <adamsiii.robert@gmail.com>
2022-08-15 12:23:28 -0700

    naming changes

--- src/routes/vision/routes.ts
+++ src/routes/vision/routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 
 import { localizeAndTranslate } from '../../controllers/vision';
-import { authorize, validate } from '../../middleware';
+import { authorize, validateReqBody } from '../../middleware';
 
 const router = Router();
 
-router.post('/objects', authorize, validate, localizeAndTranslate);
+router.post('/objects', authorize, validateReqBody, localizeAndTranslate);
 
 export default router;
