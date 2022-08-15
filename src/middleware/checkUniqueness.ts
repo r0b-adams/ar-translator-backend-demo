6265882dcc0283bec6c77cab0bfc6b8ccf601589
Robert Adams <adamsiii.robert@gmail.com>
2022-08-15 12:16:04 -0700

    updated queries with static helpers

--- src/middleware/checkUniqueness.ts
+++ src/middleware/checkUniqueness.ts
@@ -3,17 +3,20 @@ import { RequestHandler } from 'express';
 import User from '../db/models/User';
 import { UniquenessError } from '../helpers/errors';
 
+/**
+ * verify username and password not taken
+ */
 const checkUniqueness: RequestHandler = async (req, _res, next) => {
   try {
     const { username, email } = req.body;
 
-    const user_un = await User.findOne({ 'profile.username': username });
-    if (user_un) {
-      throw new UniquenessError('an account with that username already exists');
+    const usernameExists = await User.findByUsername(username);
+    if (usernameExists) {
+      throw new UniquenessError(`username ${username} not available`);
     }
 
-    const user_em = await User.findOne({ 'profile.email': email });
-    if (user_em) {
+    const emailExists = await User.findByEmail(email);
+    if (emailExists) {
       throw new UniquenessError('an account with that email already exists');
     }
 
