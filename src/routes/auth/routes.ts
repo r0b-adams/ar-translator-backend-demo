c23e958ee788ae97a125a9d4ae6ce06cf15d24cc
Robert Adams <adamsiii.robert@gmail.com>
2022-08-15 12:23:12 -0700

    naming changes

--- src/routes/auth/routes.ts
+++ src/routes/auth/routes.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
 
 import { getUser, register, login, logout } from '../../controllers/auth';
-import { authorize, validate, checkUniqueness } from '../../middleware';
+import {
+  authorize,
+  validateReqBody,
+  checkUniqueness,
+} from '../../middleware';
 
 const router = Router();
 
 router.get('/users', authorize, getUser);
-router.post('/register', validate, checkUniqueness, register);
-router.post('/login', validate, login);
+router.post('/register', validateReqBody, checkUniqueness, register);
+router.post('/login', validateReqBody, login);
 router.delete('/logout', authorize, logout);
 
 export default router;
