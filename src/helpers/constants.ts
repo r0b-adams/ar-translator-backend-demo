e865fee079c8048e63318186b0a3930b03a3b158
Robert Adams <adamsiii.robert@gmail.com>
2022-08-17 08:43:35 -0700

    Move var FILE_UPLOAD_LIMIT to constants file

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -3,6 +3,13 @@ const PASSWORD_REGEX =
   /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{12,64}$/;
 const SALT_ROUNDS = 10;
 
+const FILE_UPLOAD_LIMIT = '100mb';
 const LANGCODE_ENGLISH = 'en';
 
-export { PASSWORD_MIN_LENGTH, PASSWORD_REGEX, SALT_ROUNDS, LANGCODE_ENGLISH };
+export {
+  PASSWORD_MIN_LENGTH,
+  PASSWORD_REGEX,
+  SALT_ROUNDS,
+  LANGCODE_ENGLISH,
+  FILE_UPLOAD_LIMIT,
+};
