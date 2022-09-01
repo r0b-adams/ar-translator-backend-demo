970e2a1b27644cc3ee5ddb40c58d555bd02e4c00
Robert Adams <adamsiii.robert@gmail.com>
2022-09-01 14:01:15 -0700

    Update img schema to b64 str instead of dataURI

--- src/validation/_propSchema.ts
+++ src/validation/_propSchema.ts
@@ -6,13 +6,18 @@ import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX } from '../helpers/constants';
  * definitions for individual properties
  */
 
-export const username = Joi.string().required().messages({
-  'string.empty': `username is required`,
+export const username = Joi.string()
+  .required()
+  .messages({
+    'string.empty': `username is required`,
 });
 
-export const email = Joi.string().required().email().messages({
-  'string.empty': `email is required`,
-  'string.email': `a valid email is required`,
+export const email = Joi.string()
+  .required()
+  .email()
+  .messages({
+    'string.empty': `email is required`,
+    'string.email': `a valid email is required`,
 });
 
 export const password = Joi.string()
@@ -25,18 +30,26 @@ export const password = Joi.string()
     'string.pattern.base': `password must contain a digit, an uppercase character, a lowercase character, and at least one special character: ! @ # $ % ^ & *`,
   });
 
-export const img = Joi.string().required().dataUri().messages({
-  'string.empty': `image data required`,
-  'string.dataUri': 'valid data URI required',
+export const img = Joi.string()
+  .required()
+  .base64()
+  .messages({
+    'string.empty': `image data required`,
+    'string.base64': 'valid base64 data required',
 });
 
-export const languageCode = Joi.string().required().max(2).messages({
-  'string.max': 'valid language code required',
+export const languageCode = Joi.string()
+  .required()
+  .max(2)
+  .messages({
+    'string.max': 'valid language code required',
 });
 
 export const text = Joi.string()
   .required()
-  .messages({ 'string.empty': 'no text to translate' });
+  .messages({
+    'string.empty': 'no text to translate'
+  });
 
 export const from = languageCode.messages({
   'string.empty': 'source language is required',
@@ -46,7 +59,10 @@ export const to = languageCode.messages({
   'string.empty': 'target language is required',
 });
 
-export const b64 = Joi.string().required().base64().messages({
-  'string.empty': `data not found`,
-  'string.base64': `data must be valid base64 encoding`,
+export const b64 = Joi.string()
+  .required()
+  .base64()
+  .messages({
+    'string.empty': `data not found`,
+    'string.base64': `data must be valid base64 encoding`,
 });
